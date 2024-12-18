0ee16aa95d9bb73c0b1b5438987a4ab941f02189
Simon Prickett <simon.prickett@crate.io>
2024-12-18 18:42:52 +0000

    Renders basic wind farm outlines to map.

--- OffshoreWindFarmsDemo/Program.cs
+++ OffshoreWindFarmsDemo/Program.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using Npgsql;
+using NpgsqlTypes;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
@@ -10,21 +11,24 @@ app.UseFileServer();
 // TODO consider trapping a null here.
 await using var dataSource = NpgsqlDataSource.Create(app.Configuration.GetConnectionString("CrateDB")!);
 
-app.MapGet("/testing", async () => {
-    await using var command = dataSource.CreateCommand("SELECT id, name, description, boundaries, turbines FROM windfarms ORDER BY id ASC");
+app.MapGet("/api/windfarms", async () => {
+    await using var command = dataSource.CreateCommand("SELECT id, name, description, location, boundaries, turbines FROM windfarms ORDER BY id ASC");
     await using var reader = await command.ExecuteReaderAsync();
 
     // TODO can the database driver help with this?
-    var windFarms = new Dictionary<string, Object>();
+    var windFarms = new List<Object>();
 
     while (await reader.ReadAsync()) {
-        var coords = reader.GetFieldValue<JsonDocument>(3);
-        Console.WriteLine(coords);
-        windFarms.Add(reader.GetString(0), new {
-            name = reader.GetString(1),
-            boundaries = reader.GetFieldValue<JsonDocument>(3),
-            turbines = reader.GetFieldValue<JsonDocument>(4)
-        });
+        windFarms.Add(
+            new {
+                id = reader.GetString(0),
+                name = reader.GetString(1),
+                description = reader.GetString(2),
+                location = reader.GetFieldValue<NpgsqlPoint>(3),
+                boundaries = reader.GetFieldValue<JsonDocument>(4),
+                turbines = reader.GetFieldValue<JsonDocument>(5)
+            }
+        );
     }
 
     return new {
--- OffshoreWindFarmsDemo/wwwroot/js/app.js
+++ OffshoreWindFarmsDemo/wwwroot/js/app.js
@@ -1,1 +1,30 @@
-alert('hello from app.js');
+const myMap = L.map('mapId').setView([54.91451400766527, -3.5375976562500004], 6);
+
+async function showAllWindFarms() {
+  const response = await fetch('/api/windfarms');
+  const responseDoc = await response.json();
+  
+  for (const windFarm of responseDoc.results) {
+    const boundaries = windFarm.boundaries;
+    const geoJSON = {
+      type: 'Feature',
+      properties: {
+        // TODO
+      },
+      geometry: boundaries
+    };
+
+    console.log(windFarm.location);
+    L.geoJSON(geoJSON).addTo(myMap);
+  }
+}
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+  {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }
+).addTo(myMap);
+
+showAllWindFarms();
