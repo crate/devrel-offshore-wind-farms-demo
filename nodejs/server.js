08b8d15ad31610d04d1b8e2d6a12d261ba265355
Simon Prickett <simon.prickett@crate.io>
2025-01-17 21:04:30 +0000

    Scaffolded all the routes.

--- nodejs/server.js
+++ nodejs/server.js
@@ -8,7 +8,27 @@ const app = express();
 app.use(express.static('static'));
 app.use(express.json());
 
+app.get('/api/windfarms', async (req, res) => {
+  res.sendStatus('TODO');
+});
 
+app.get('/api/latest/:id', async (req, res) => {
+  res.sendStatus('TODO');
+});
+
+app.get('/api/avgpctformonth/:id/:ts', async (req, res) => {
+  res.sendStatus('TODO');
+});
+
+app.get('/api/outputforday/:id/:ts', async (req, res) => {
+  res.sendStatus('TODO');
+});
+
+app.get('/api/dailymaxpct/:id/:days', async (req, res) => {
+  res.sendStatus('TODO');
+});
+
+// 
 // Start the Express server.
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}.`);
