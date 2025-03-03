3d6a9531e891bff4180abcb7c41871e2b963c034
Simon Prickett <simon.prickett@crate.io>
2025-03-03 12:31:14 +0000

    Work in progress.

--- java/windfarms/src/main/java/com/cratedb/windfarms/App.java
+++ java/windfarms/src/main/java/com/cratedb/windfarms/App.java
@@ -4,9 +4,6 @@ import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
 
-/**
- * Hello world!
- */
 public class App extends Application<WindFarmsConfiguration> {
     public static void main(String[] args) throws Exception {
         new App().run(args);
