4da0277e134e746b66490e96b8703d3b1b7fb16f
Simon Prickett <simon.prickett@crate.io>
2025-03-03 12:06:47 +0000

    Some more DropWizard setup.

--- java/windfarms/src/main/java/com/cratedb/windfarms/App.java
+++ java/windfarms/src/main/java/com/cratedb/windfarms/App.java
@@ -1,10 +1,29 @@
 package com.cratedb.windfarms;
 
+import io.dropwizard.core.Application;
+import io.dropwizard.core.setup.Bootstrap;
+import io.dropwizard.core.setup.Environment;
+
 /**
  * Hello world!
  */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class App extends Application<WindFarmsConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new App().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "windfarms";
+    }
+
+    @Override
+    public void initialize(Bootstrap<WindFarmsConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(WindFarmsConfiguration configuration, Environment environment) {
+        // nothing to do yet
     }
 }
---
+++ java/windfarms/src/main/java/com/cratedb/windfarms/WindFarmsConfiguration.java
@@ -0,0 +1,33 @@
+package com.cratedb.windfarms;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.core.Configuration;
+import jakarta.validation.constraints.NotEmpty;
+
+public class WindFarmsConfiguration extends Configuration {
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "windfarms";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
+}
